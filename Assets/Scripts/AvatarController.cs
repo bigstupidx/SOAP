fd3e0c68cf615c0b143bf6c334f804e09dd637b3
lconeill <luke.c.oneill@gmail.com>
2016-03-22 21:55:54 -0400

    Snake mechanic complete
    -player collects "grow" elements which adds tails
    -tails follow the head
    -playable in the gamescreen scene

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 
 public class AvatarController : MonoBehaviour {
 
-    private float previous_avatar_position;     // The avatars position during the last frame
-    private float current_avatar_position;      // The avatars position during the current frame
+    private float min_distance = 0.65f;         // The minimum distance between elements in the snake
+    private Vector3 previous_avatar_position;   // The avatars position when it travelled min_distance 
     private string avatar_direction;            // The direction the avatar is moving
-    private int speed;                          // The speed at which the avatar moves
+    private Vector3 avatar_vector_direction;    // The avatar vector direction
+    private int avatar_speed;                   // The speed at which the avatar moves
+    private Vector2 default_avatar_position = new Vector2(0, -2);   // The avatars starting position
+    public TailMovement tail_movement_script;
     Dictionary<string, string> cw_movement = new Dictionary<string, string>();  // Defines the next direction for clockwise turn
     Dictionary<string, string> ccw_movement = new Dictionary<string, string>(); // Defines the next direction for counter-clockwise turn
-    private Vector2 default_avatar_position = new Vector2(0, -2);   // The avatar starting position
-    private int avatar_speed;   // The avatars movement speed
 
-   
 
 	// Use this for initialization
 	void Start () 
@@ -21,6 +21,7 @@ public class AvatarController : MonoBehaviour {
         avatar_direction = "up";
         transform.position = default_avatar_position;
         avatar_speed = 2;
+        previous_avatar_position = transform.position - new Vector3(0,min_distance,0);
 
         cw_movement.Add("up", "right");
         cw_movement.Add("right", "down");
@@ -31,19 +32,24 @@ public class AvatarController : MonoBehaviour {
         ccw_movement.Add("left", "down");
         ccw_movement.Add("down", "right");
         ccw_movement.Add("right", "up");
+
+        avatar_vector_direction = Vector3.up;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        moveAvatar(avatar_direction);
+        moveAvatar();
+        
+        float current_distance = Vector3.Distance(transform.position, previous_avatar_position);
+
+        // Update tails if the avatar has travelled min_distance
+        if (current_distance >= min_distance)
+        {
+            tail_movement_script.tailFollow(previous_avatar_position);
+            previous_avatar_position = transform.position;
+        }
 	}
-
-
-    public void testPrint()
-    {
-        Debug.Log("Execute test!");
-    }
 
 
     // Turn the avatar clockwise (true) or counterclockwise (false)
@@ -52,36 +58,45 @@ public class AvatarController : MonoBehaviour {
         if (rotation)
         {
             avatar_direction = cw_movement[avatar_direction];
-            moveAvatar(avatar_direction);
+            determineDirection(avatar_direction);
+            moveAvatar();
         }
         else
         {
             avatar_direction = ccw_movement[avatar_direction];
-            moveAvatar(avatar_direction);
+            determineDirection(avatar_direction);
+            moveAvatar();
+        }
+    }
+
+
+    // The vector direction the avatar must move
+    public void determineDirection(string direction)
+    {
+        switch (direction)
+        {
+            case "up":
+                avatar_vector_direction = Vector3.up;
+                break;
+
+            case "right":
+                avatar_vector_direction = Vector3.right;
+                break;
+
+            case "left":
+                avatar_vector_direction = Vector3.left;
+                break;
+
+            case "down":
+                avatar_vector_direction = Vector3.down;
+                break;
         }
     }
 
 
     // Move the avatar in the direction specified
-    public void moveAvatar(string direction)
+    public void moveAvatar()
     {
-        switch (direction)
-        {
-            case "up":
-                transform.position += Vector3.up * Time.deltaTime * avatar_speed;
-                break;
-
-            case "right":
-                transform.position += Vector3.right * Time.deltaTime * avatar_speed;
-                break;
-
-            case "left":
-                transform.position += Vector3.left * Time.deltaTime * avatar_speed;
-                break;
-
-            case "down":
-                transform.position += Vector3.down * Time.deltaTime * avatar_speed;
-                break;
-        }
+        transform.position += avatar_vector_direction * Time.deltaTime * avatar_speed;
     }
 }
---
+++ Assets/Scripts/TailMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TailMovement : MonoBehaviour {
+
+    List<GameObject> tail_elements = new List<GameObject>();    // Used to reorder tails to mimic snake movement
+    public GameObject[] tail_sprites;   // Tail sprites to set active when a grow element has been eaten
+    public bool grow_tail = false;  // True if the avatar has eaten a grow element since the last minimum distance covered
+    private int tail_count = 0;     // The amount of tails collected. Used to access tail elements in tail_sprites array
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    // Place the last tail in the gap created when the avatar moves the minimum distance (specified in AvatarController)
+    public void tailFollow(Vector3 position)
+    {
+        if (grow_tail)
+        {
+            GameObject tail = tail_sprites[tail_count];
+            tail.SetActive(true);
+            tail_elements.Insert(0, tail);
+            tail.transform.position = position;
+            grow_tail = false;
+            tail_count++;
+
+            //foreach (GameObject obj in tail_elements)
+            //{
+            //    Debug.Log(obj.name);
+            //}
+        }
+
+        else if (tail_elements.Count != 0)
+        {
+            int last_index = tail_elements.Count - 1;
+            GameObject last_tail = tail_elements[last_index];
+            last_tail.transform.position = position;
+            tail_elements.RemoveAt(last_index);
+            tail_elements.Insert(0, last_tail);
+
+            //foreach (GameObject obj in tail_elements)
+            //{
+            //    Debug.Log(obj.name);
+            //}
+        }
+    }
+}
