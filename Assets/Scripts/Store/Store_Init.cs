e14d60ce9996fefcf78b3b906cab3f2b9b6ee576
lconeill <luke.c.oneill@gmail.com>
2016-03-27 22:04:51 -0400

    Soomla store package
    -added soomla store and core package

---
+++ Assets/Scripts/Store/Store_Init.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Soomla.Store;
+
+public class Store_Init : MonoBehaviour 
+{
+	public GameObject image_ref;
+	private Animator image_anim;
+	private Animation fadeColorAnimationClip;
+	
+	// Use this for initialization
+	void Start () 
+	{	
+		SoomlaStore.Initialize(new MayhemStoreAssets());
+	}
+	
+
+	
+	public void LoadDelayed()
+	{
+		//Load the selected scene, by scene index number in build settings
+        //Application.LoadLevel(1); // Load the classic start screen
+	}
+}
