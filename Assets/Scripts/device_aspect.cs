f7906cf074bcc4a92d7e50197c6d2e3325874b2c
Jason Coelho <jason.coelho@me.com>
2016-03-31 21:04:40 -0400

    First Commit
    Committing prefabs for tile tests and first scripts for tile spawning.
    
    Including camera script to set a fixed aspect ratio for the gameplay
    screen real-estate

---
+++ Assets/Scripts/device_aspect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class device_aspect : MonoBehaviour 
+{
+	
+	public float targetAspect;
+	
+	void Start () 
+	{
+		float windowAspect = (float)Screen.width / (float)Screen.height;
+		float scaleHeight = windowAspect / targetAspect;
+		Camera camera = GetComponent<Camera>();
+		
+		if (scaleHeight < 1.0f)
+		{  
+			camera.orthographicSize = camera.orthographicSize / scaleHeight;
+		}
+	}
+}
---
+++ Assets/Scripts/tile_size.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tile_size : MonoBehaviour 
+{
+	
+	Vector3 rect_srpite;
+	SpriteRenderer rect_srpite_renderer;
+	public GameObject second_tile_ref;
+	Transform second_tile_transform;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		rect_srpite_renderer = this.gameObject.GetComponent<SpriteRenderer>();
+		rect_srpite = rect_srpite_renderer.bounds.size;
+	    second_tile_transform = second_tile_ref.GetComponent<Transform>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		Debug.Log("The Dimentions of the tile are: " + rect_srpite);
+		Debug.Log("Second Tile Position Y is: " + this.gameObject.transform.position.y);
+		
+		second_tile_transform.position = new Vector3(0, this.gameObject.transform.position.y + rect_srpite.y, 0);
+
+	}
+	
+}
