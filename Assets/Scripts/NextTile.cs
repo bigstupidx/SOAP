134ea09d986a35aa43d287796e9b0da1806453ce
Jason Coelho <jason.coelho@me.com>
2016-04-03 17:44:37 -0400

    Game screen update
    Created new script to check for collisions between the player and
    objects in the scene and one for a trigger on the tiles to spawn the
    next one in the list.
    
    Changed a few game objects around and made a few new prefabs.

---
+++ Assets/Scripts/NextTile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextTile : MonoBehaviour 
+{
+	private GameObject tile_spawner_ref;
+
+	private spawn_tiles spawn_tiles_script;
+
+	// Use this for initialization
+	void Start () 
+	{
+		tile_spawner_ref = GameObject.Find("Tile_Spawner");
+		spawn_tiles_script =  tile_spawner_ref.GetComponent<spawn_tiles>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) 
+	{
+		// If the Collider2D component is enabled on the object we collided with
+		if (coll.gameObject.tag == "Player")
+		{
+			spawn_tiles_script.move_tile();
+		}
+	}
+}
