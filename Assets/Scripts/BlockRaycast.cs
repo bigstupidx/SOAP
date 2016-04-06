aeea11ead1f97e72dc2334b08eed822d03a4ab66
lconeill <luke.c.oneill@gmail.com>
2016-04-05 21:17:14 -0400

    Block turn on UI touch
    -added code to stop the avatar from turning when UI element has been
    clicked

---
+++ Assets/Scripts/BlockRaycast.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class BlockRaycast : MonoBehaviour {
+
+    public static bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        // TODO: Investigate further - I don't know why only GUI elements return a count of greater than 1
+        return results.Count > 1;
+    }
+}
