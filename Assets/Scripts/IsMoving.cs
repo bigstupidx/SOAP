a8b30fd3e31a76746f23d68baffeba9d64129406
Jason Coelho <jason.coelho@me.com>
2016-04-11 21:59:10 -0400

    Update on obstacles
    Continued with obstacles, spinning and moving objects now work through
    obstacle manager and made changes to the challenge room script

---
+++ Assets/Scripts/IsMoving.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsMoving : MonoBehaviour 
+{
+	public bool is_moving_right = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		// Obstacle collision: the player "dies", avatar setActive is set to false.
+        if (coll.gameObject.tag == "obstacle")
+        {
+			if (is_moving_right == true)
+			{
+				is_moving_right = false;
+			}
+
+			else
+			{
+				is_moving_right = true;
+			}
+        }
+	}
+}
---
+++ Assets/Scripts/RegularTiles.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegularTiles : MonoBehaviour 
+{
+	public GameObject[] spinning_obj_list;
+	public GameObject[] moving_obj_list;
+
+	public GameObject obstacle_manager_ref;
+	private ObstacleManager obstacle_manager_script;
+
+	// Use this for initialization
+	void Start () 
+	{
+		obstacle_manager_script = obstacle_manager_ref.GetComponent<ObstacleManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (spinning_obj_list.Length > 0)
+		{	
+			obstacle_manager_script.spinWalls();
+		}
+
+		if (moving_obj_list.Length > 0)
+		{
+			obstacle_manager_script.moveWalls();
+		}
+	}
+}
