c540d907c637652bc1ed802bd1ce3c8ad856d09a
Jason Coelho <jason.coelho@me.com>
2016-04-13 20:20:27 -0400

    Update Obstacle manager
    changing how obstacle manager and regular tile script use their game
    object lists.

--- Assets/Scripts/RegularTiles.cs
+++ Assets/Scripts/RegularTiles.cs
@@ -4,10 +4,6 @@ using System.Collections.Generic;
 
 public class RegularTiles : MonoBehaviour 
 {
-	public GameObject[] spinning_obj_list;
-	public GameObject[] moving_obj_listLR;
-	public GameObject[] moving_obj_listUD;
-	public GameObject[] falling_obj_list;
 	public GameObject obstacle_manager_ref;
 	private ObstacleManager obstacle_manager_script;
 	private List<Vector3> falling_obj_position_list = new List<Vector3>();
@@ -16,7 +12,7 @@ public class RegularTiles : MonoBehaviour
 	void Start () 
 	{
 		obstacle_manager_script = obstacle_manager_ref.GetComponent<ObstacleManager>();
-		foreach(GameObject item in falling_obj_list)
+		foreach(GameObject item in obstacle_manager_script.falling_obstacles)
 		{
 			falling_obj_position_list.Add(item.transform.position);
 		}
@@ -26,34 +22,29 @@ public class RegularTiles : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (spinning_obj_list.Length > 0)
+		if (obstacle_manager_script.spinning_obstacles.Length > 0)
 		{	
 			obstacle_manager_script.spinWalls();
 		}
 
-		if (moving_obj_listLR.Length > 0)
+		if (obstacle_manager_script.moving_wall_obstaclesLR.Length > 0)
 		{
 			obstacle_manager_script.moveWallsLR();
 		}
 
-		if (moving_obj_listUD.Length > 0)
+		if (obstacle_manager_script.moving_wall_obstaclesUD.Length > 0)
 		{
 			obstacle_manager_script.moveWallsUD();
 		}
 
-		if (moving_obj_listUD.Length > 0)
+		if (obstacle_manager_script.falling_obstacles.Length > 0)
 		{
-			obstacle_manager_script.moveWallsUD();
-		}
-
-		if (falling_obj_list.Length > 0)
-		{
-			if (falling_obj_list[0].activeSelf == false)
+			if (obstacle_manager_script.falling_obstacles[0].activeSelf == false)
 			{
-				for (int i = 0; i < falling_obj_list.Length; i++)
+				for (int i = 0; i < obstacle_manager_script.falling_obstacles.Length; i++)
 				{
-					falling_obj_list[i].transform.position = falling_obj_position_list[i];
-					falling_obj_list[i].GetComponent<Rigidbody2D>().isKinematic = true;
+					obstacle_manager_script.falling_obstacles[i].transform.position = falling_obj_position_list[i];
+					obstacle_manager_script.falling_obstacles[i].GetComponent<Rigidbody2D>().isKinematic = true;
 				}
 			}
 
