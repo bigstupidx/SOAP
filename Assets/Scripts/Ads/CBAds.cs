1c884b50e50d405c4d75c46a41c549c7a16c309b
lconeill <luke.c.oneill@gmail.com>
2016-04-28 22:04:02 -0400

    chartboost ad integration
    -got new soomla packages (packages from before were corrupt)
    -integrated chartboost ads

---
+++ Assets/Scripts/Ads/CBAds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using ChartboostSDK;
+
+public class CBAds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Show the ads when I click the button
+    public void showAds()
+    {
+        Chartboost.showInterstitial(CBLocation.HomeScreen);
+    }
+}
