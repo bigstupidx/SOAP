e331e7c1874f9693b00eff8a89780c58c5e384be
lconeill <luke.c.oneill@gmail.com>
2016-05-18 22:23:17 -0400

    trigger each ball individually
    -logic to add force to groups of balls separately

--- Assets/Scripts/BallTrigger.cs
+++ Assets/Scripts/BallTrigger.cs
@@ -1,87 +1,144 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 
 //
-// This script is on the tile prefab and the obstacles are put into their corresponding arrays
-// for further manipulation in the code
+// Logic for having falling obstacles be triggered individually 
+//
 
 public class BallTrigger : MonoBehaviour {
 
-    public GameObject[] ball_triggers;
-    public GameObject[] ball_groups;
-    public GameObject random;
+    public GameObject[] ball_trigger_go;
+    public GameObject avatar;
 
-    public float force_mag;
-
-    private Vector2 force_push_left;
+    private Vector2 force_push_left = new Vector2(1.0f, 0.0f);
     private Vector2 force_push_right;
     private Vector2 force_push_up;
     private float[] force_array;
+    private BoxCollider2D[] trigger_box_collider;
+    private List<GameObject[]> ball_elements = new List<GameObject[]>();
 
-    public GameObject avatar;
+    
 
 
     // Use this for initialization
 	void Start () 
 	{
-        getBallChildren(random);
+        trigger_box_collider = new BoxCollider2D[ball_trigger_go.Length];
+
+        for (int i = 0; i < ball_trigger_go.Length; i++)
+        {
+            trigger_box_collider[i] = ball_trigger_go[i].GetComponent<BoxCollider2D>();
+        }
+
+        for (int j = 0; j < ball_trigger_go.Length; j++)
+        {
+            ball_elements.Add(getBallChildren(ball_trigger_go[j]));
+        }
+
+        //foreach (GameObject[] go in ball_elements)
+        //{
+        //    foreach(GameObject ball in go){
+        //        Debug.Log(ball.name);
+        //    }
+        //}
 	}
 	
+
 	// Update is called once per frame
 	void Update () 
 	{
-        if (obstacle_manager_script.falling_obstacles.Length > 0)
+
+        for (int i = 0; i < trigger_box_collider.Length; i++)
         {
-            if (obstacle_trigger.IsTouching(the_player.GetComponent<CircleCollider2D>()))
+            if (trigger_box_collider[i].IsTouching(avatar.GetComponent<CircleCollider2D>()))
             {
-                obstacle_manager_script.dropBalls();
+                //foreach (GameObject ball in ball_elements[i])
+                //{
+                //    Debug.Log(ball.name);
+                //}
+
+                int force = getForce(ball_trigger_go[i].name);
+                dropBalls(ball_elements[i], force);
+
+                
+                
             }
-
-            else if (obstacle_trigger_reset.IsTouching(the_player.GetComponent<CircleCollider2D>()))
-            {
-                for (int i = 0; i < obstacle_manager_script.falling_obstacles.Length; i++)
-                {
-                    obstacle_manager_script.falling_obstacles[i].gameObject.transform.position = falling_obj_position_list[i];
-                    obstacle_manager_script.falling_obstacles[i].gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                }
-            }
-        }
-	}
-
-
-    private void getBallChildren(GameObject random)
-    {
-        foreach (Transform child in random.transform)
-        {
-            Debug.Log(child.gameObject.name);
         }
     }
 
 
-    //public void dropBalls()
-    //{
-    //    for (int i = 0; i < falling_obstacles.Length; i++)
-    //    {
-    //        if(falling_obstacles[i].transform.parent.parent.parent.gameObject.activeSelf == true)
-    //        {
-    //            falling_obstacles[i].GetComponent<Rigidbody2D>().isKinematic = false;
 
-    //            if(falling_obstacles[i].transform.parent.gameObject.tag == "force_push_left")
-    //            {
-    //                falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_left);
-    //            }
+    // Return an array of all children under the trigger GameObject
+    private GameObject[] getBallChildren(GameObject trigger)
+    {
+        int ball_count = trigger.transform.childCount;
+        GameObject[] balls = new GameObject[ball_count];
+        
+        int counter = 0;
 
-    //            if(falling_obstacles[i].transform.parent.gameObject.tag == "force_push_right")
-    //            {
-    //                falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_right);
-    //            }
+        foreach (Transform child in trigger.transform)
+        {
+            balls[counter] = child.gameObject;
+            counter++;
+        }
 
-    //            if(falling_obstacles[i].transform.parent.gameObject.tag == "force_push_up")
-    //            {
-    //                falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_up);
-    //            }
-    //        }
-    //    }
-    //}
+        return balls;
+    }
+
+
+    // Extract the force magnitude from the trigger name
+    private int getForce(string trigger_name)
+    {
+        Match match = Regex.Match(trigger_name, @"_fx[0-9]+", RegexOptions.IgnoreCase);
+        int force = 0;
+
+        if (match.Success)
+        {
+            string key = match.Groups[0].Value;
+            string force_str = key.Split(new string[] {"fx"}, StringSplitOptions.None)[1];
+            force = Convert.ToInt32(force_str);
+        }
+
+        return force;
+    }
+
+
+    private void dropBalls(GameObject[] ball_array, int force)
+    {
+        foreach (GameObject ball in ball_array)
+        {
+            Debug.Log(ball.name);
+            Debug.Log(force);
+
+            ball.GetComponent<Rigidbody2D>().isKinematic = false;
+            ball.GetComponent<Rigidbody2D>().AddForce(force_push_left*force);
+        }
+
+        //for (int i = 0; i < falling_obstacles.Length; i++)
+        //{
+        //    if (falling_obstacles[i].transform.parent.parent.parent.gameObject.activeSelf == true)
+        //    {
+        //        falling_obstacles[i].GetComponent<Rigidbody2D>().isKinematic = false;
+
+        //        if (falling_obstacles[i].transform.parent.gameObject.tag == "force_push_left")
+        //        {
+        //            falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_left);
+        //        }
+
+        //        if (falling_obstacles[i].transform.parent.gameObject.tag == "force_push_right")
+        //        {
+        //            falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_right);
+        //        }
+
+        //        if (falling_obstacles[i].transform.parent.gameObject.tag == "force_push_up")
+        //        {
+        //            falling_obstacles[i].GetComponent<Rigidbody2D>().AddForce(force_push_up);
+        //        }
+        //    }
+        //}
+    }
 }
