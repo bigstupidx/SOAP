cd496710c13e7a3ab584f071594aaab9e8436fa2
lconeill <luke.c.oneill@gmail.com>
2016-07-11 21:55:25 -0400

    Rewarded avatars
    -added test object sprites to avatar and tail scroll list for testing
    rewarded avatars
    -created rewarded avatars script

---
+++ Assets/Scripts/RewardedAvatars.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewardedAvatars : MonoBehaviour {
+
+
+    public readonly string[] rewarded_avatar_name_list = { "default_avatar, default_tail, cyborg_avatar, cyborg_tail" };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
